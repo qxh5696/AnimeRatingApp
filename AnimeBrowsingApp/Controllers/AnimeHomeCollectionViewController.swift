e60fe2cf8dcd0b980393695f4d767a5c91416676
Qadir Haqq <haqq.q173@gmail.com>
2020-07-20 20:12:49 -0700

    Async BlockOperations WIP

---
+++ AnimeBrowsingApp/Controllers/AnimeHomeCollectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AnimeHomeCollectionViewController.swift
+//  AnimeBrowsingApp
+//
+//  Created by qh on 7/20/20.
+//  Copyright © 2020 qadir haqq. All rights reserved.
+//
+
+import UIKit
+
+class AnimeHomeCollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
